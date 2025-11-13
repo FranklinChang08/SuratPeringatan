3434c07c48fa601468b5141a62c157a177f8307c
Ririn Anisa <ririnanisasihite23@gmail.com>
2025-11-13 18:01:40 +0700

    ubah surat

--- staff/surat.php
+++ staff/surat.php
@@ -10,7 +10,6 @@
     <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.css">
     <script src="../node_modules/bootstrap/dist/js/bootstrap.js"></script>
 
-<<<<<<< HEAD
   <link rel="stylesheet" href="../static/style/font.css">
   <link rel="stylesheet" href="../static/style/sidebar.css">
 
@@ -203,98 +202,6 @@
     }
 
   </style>
-=======
-    <link rel="stylesheet" href="../static/style/font.css">
-    <link rel="stylesheet" href="../static/style/sidebar.css">
-    <link rel="stylesheet" href="../static/style/dashboard.css">
-    <link rel="stylesheet" href="../static/style/surat.css">
-
-    <style>
-    /* Buat custom backdrop */
-    .modal-backdrop {
-        background-color: rgba(0, 0, 0, 0.8) !important;
-        backdrop-filter: blur(1000px) !important;
-        -webkit-backdrop-filter: blur(1000px) !important;
-    }
-
-    .modal-backdrop.show {
-        background-color: rgba(0, 0, 0, 0.25);
-        backdrop-filter: blur(100px);
-        -webkit-backdrop-filter: blur(100px);
-    }
-
-    .modal-backdrop {
-        transition: opacity 0.3s ease;
-    }
-
-    .card-group .container {
-        background-color: white;
-        padding: 0.5rem;
-        border-radius: 8px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-        margin-bottom: 1rem;
-    }
-
-    .card-group .container .button {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-
-    .card-group .container .button .button-group {
-        display: flex;
-        gap: 8px;
-    }
-
-    .card-group .container .button .btnTambah,
-    .card-group .container .button .btnImport {
-        padding: 0.5rem 1rem;
-        border: none;
-        border-radius: 4px;
-        font-size: 1rem;
-        font-weight: 600;
-    }
-
-    .card-group .container .button .btnTambah {
-        display: flex;
-        align-items: center;
-        gap: 0.5rem;
-        background-color: var(--primary);
-        color: white;
-    }
-
-    .card-group .container .button .btnImport {
-        display: flex;
-        align-items: center;
-        gap: 0.5rem;
-        background-color: var(--primary);
-        color: white;
-    }
-
-    .card-group .container .button .form-search {
-        display: flex;
-        align-items: center;
-        gap: 0.5rem;
-        border: 1px solid #ccc;
-        padding: 0 0.5rem;
-        border-radius: 8px;
-    }
-
-    .card-group .container .button .form-search label {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        color: rgba(0, 0, 0, 0.5);
-    }
-
-    .card-group .container .button form input {
-        padding: 0.5rem;
-        border: none;
-        outline: none;
-        background-color: transparent;
-    }
-    </style>
->>>>>>> f472453ee334d9bad97829eea92cac2f71090106
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
@@ -308,7 +215,6 @@
   include('../component/sidebar.php')
   ?>
 
-<<<<<<< HEAD
   <div class="main-content">
     <header class="header">
       <h2 class="fw-bold">Data Surat Peringatan</h2>
@@ -426,24 +332,6 @@
         </div>
       </div>
     </div>
-=======
-    <div class="main-content">
-        <header class="header">
-            <h2 class="fw-bold mb-0">Data Mahasiswa</h2>
-            <div class="account">
-                <div class="account-desc">
-                    <h2 class="nama fs-6 mb-0 fw-bold">Gilang</h2>
-                    <h2 class="email">gilang@gmail.com</h2>
-                </div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none"
-                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                    class="lucide lucide-user-icon lucide-user">
-                    <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                    <circle cx="12" cy="7" r="4" />
-                </svg>
-            </div>
-        </header>
->>>>>>> f472453ee334d9bad97829eea92cac2f71090106
 
         <section id="card-group" class="card-group">
 
